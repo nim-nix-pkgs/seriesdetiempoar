0546164b3673637de2efca358ab991cfb6c004e7
hugosenari <hugosenari@gmail.com>
2022-03-12 04:03:36 -0300

    chore: re index seriesdetiempoar

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Series de Tiempo de Argentina Government MultiSync API Client for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-seriesdetiempoar-master.flake = false;
+  inputs.src-seriesdetiempoar-master.owner = "juancarlospaco";
+  inputs.src-seriesdetiempoar-master.ref   = "refs/heads/master";
+  inputs.src-seriesdetiempoar-master.repo  = "nim-seriesdetiempoar";
+  inputs.src-seriesdetiempoar-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-seriesdetiempoar-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-seriesdetiempoar-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
